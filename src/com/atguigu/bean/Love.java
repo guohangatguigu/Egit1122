95a8a9a4ad2b87d7753e0345a585a1e2e7d3603b
郭航 <910357212@qq.com>
2020-03-21 20:36:38 +0800

    我也喜欢你！！！--袁姗姗

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -3,5 +3,6 @@ package com.atguigu.bean;
 public class Love {
 	public static void main(String[] args) {
         System.out.println("我喜欢你，袁姗姗！！！");
+        System.out.println("我也喜欢你！！！--袁姗姗");
 	}
 }
